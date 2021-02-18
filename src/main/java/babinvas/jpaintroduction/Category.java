c0b6c2cbfb75250a918aa53e4ff4aae04b3b5ef2
babinvas <babinvas@gmail.com>
2021-02-18 20:09:41 +0300

    Initial commit

---
+++ src/main/java/babinvas/jpaintroduction/Category.java
@@ -0,0 +1,14 @@
+package babinvas.jpaintroduction;
+
+public class Category {
+	private Long id;
+	private String title;
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+}
---
+++ src/main/java/babinvas/jpaintroduction/Topic.java
@@ -0,0 +1,23 @@
+package babinvas.jpaintroduction;
+
+public class Topic {
+	private Long id;
+	private String title;
+	private Category category;
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+}
