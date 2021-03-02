2a608358da152dc07dcc8a30519b082e5ba80b17
babinvas <babinvas@gmail.com>
2021-03-02 18:57:45 +0300

    Test methods have been added to the main test class

--- src/test/java/babinvas/jpaintroduction/AppTest.java
+++ src/test/java/babinvas/jpaintroduction/AppTest.java
@@ -1,12 +1,10 @@
 package babinvas.jpaintroduction;
 
 import babinvas.jpaintroduction.model.Category;
+import babinvas.jpaintroduction.model.Topic;
 import org.junit.jupiter.api.*;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
+import javax.persistence.*;
 
 public class AppTest {
 	EntityManager entityManager;
@@ -24,13 +22,44 @@ public class AppTest {
 	@Test
 	public void shouldPersistCategory() {
 		Category cat = new Category();
-		cat.setTitle("new category");
+		cat.setTitle("test");
+
+		entityManager.persist(cat);
+	}
+
+	@Test
+	public void shouldFindCategory() {
+		Category cat = new Category();
+		cat.setTitle("test");
 
 		entityManager.persist(cat);
 
 		Category result = entityManager.find(Category.class, 1L);
 
 		Assertions.assertNotNull(result);
+	}
+
+	@Test
+	public void shouldPersistCategoryAndTopics() {
+		Category cat = new Category();
+		cat.setTitle("test");
+
+		Topic topic = new Topic();
+		topic.setTitle("topic");
+		topic.setCategory(cat);
+
+		entityManager.persist(cat);
+	}
+
+	@Test
+	public void shouldPerformQuery() {
+		Category cat = new Category();
+		cat.setTitle("query");
+
+		entityManager.persist(cat);
+
+		Query query = entityManager.createQuery("SELECT c from Category c WHERE c.title = 'query'");
+		Assertions.assertNotNull(query.getSingleResult());
 	}
 
 	@AfterEach
